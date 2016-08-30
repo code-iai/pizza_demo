8b48102c936551dff5b74dd8d2603678424cbfae
Mihai Pomarlan <mpomarlan@yahoo.co.uk>
2016-08-30 09:21:16 +0200

    Min.fix.

--- scripts/test_prac2cram_RPC.py
+++ scripts/test_prac2cram_RPC.py
@@ -16,7 +16,7 @@ remote_server = rpc_client.get_proxy()
 ## Cutting test: slice a particular region
 
 cut_a_slice = {'action_cores': [{'action_core_name': 'Cut_Test', 'action_roles': [{'role_name': 'utensil', 'role_value': 'pizza_cutter'}, {'role_name': 'unit', 'role_value': 'slice'}, {'role_name': 'obj_to_be_cut', 'role_value': 'pizza'}, {'role_name': 'action_verb', 'role_value': 'cut'}, {'role_name': 'amount', 'role_value': '1'}]}]}
-neutralization_tasks = [cut_a_slice]
+cut_test = [cut_a_slice]
 
 # call a method called 'reverse_string' with a single string argument
 result = remote_server.prac2cram_client(cut_test)
